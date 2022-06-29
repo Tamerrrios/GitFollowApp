00ede92c997d01f2e573d589bf02c7d3489f1200
Temur <tamerrrios@gmail.com>
2022-06-29 21:24:14 +0500

    Initial Commit

---
+++ GitFollowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitFollowApp
+//
+//  Created by Temur Juraev on 29.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
