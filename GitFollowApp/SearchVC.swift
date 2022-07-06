dc6005e354005e8463d032e5f87cc2ce6e19a299
Temur <tamerrrios@gmail.com>
2022-07-06 18:11:10 +0500

    initial settings

---
+++ GitFollowApp/SearchVC.swift
@@ -0,0 +1,22 @@
+//
+//  SearchVC.swift
+//  GitFollowApp
+//
+//  Created by Temur Juraev on 29.06.2022.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+
+    }
+    
+
+
+
+}
