7956486f21ef508c320ca1e49e7cf36aaed6b279
Temur <tamerrrios@gmail.com>
2022-07-10 23:03:18 +0500

    some issues fixing

---
+++ GitFollowApp/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GitFollowApp
+//
+//  Created by Temur Juraev on 10.07.2022.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
